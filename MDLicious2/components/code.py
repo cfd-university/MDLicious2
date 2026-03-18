81b5770145701ef81513744e6f73e5dc18950aa4
tomrobin-teschner <tomrobin.teschner@yahoo.de>
2026-03-18 19:26:53 +0000

    added code syntax highlighting

---
+++ MDLicious2/components/code.py
@@ -0,0 +1,38 @@
+from MDLicious2.components.base import Component
+from os.path import join
+import re
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+class Code(Component):
+    def __init__(self, content):
+        super().__init__(content)
+
+    def match(self, index):
+        line = self.content[index]
+        return re.search(r'^```[A-Za-z0-9+]+\s*$', line) 
+
+    def convert(self, index):
+        start, end = self._find_start_end_based_on_pattern(index, '```')
+
+        language = 'text'
+        if len(self.content[index].strip()) > 3:
+            language = self.content[index].strip()[3:]
+
+        lexer = get_lexer_by_name(language)
+        formatter = HtmlFormatter(style='nord', linenos="table", cssclass="codehilite")
+        css = formatter.get_style_defs('.codehilite')
+
+        with open('code.css', 'w') as f:
+            f.write(css)
+
+        code = ''
+        for i in range(start, end):
+            code += '\n' + self.content[i]
+        code += '\n'
+
+        html_code = highlight(code, lexer, formatter)
+
+        return html_code
