201c048f9040e203352a6be6ad7f1b243c724b4a
tomrobin-teschner <tomrobin.teschner@yahoo.de>
2026-03-18 19:21:58 +0000

    outline of converter

---
+++ MDLicious2.py
@@ -0,0 +1,30 @@
+from MDLicious2 import CommandLineArguments, FileProcessor, Mark2HTML, Preprocessor
+from MDLicious2 import ComponentManager, YouTube, EquationEnvironment, Code
+
+
+def main():
+    # process input file
+    args = CommandLineArguments()
+    filereader = FileProcessor(args)
+
+    # register customised markdown to html converters
+    component_manager = ComponentManager()
+    component_manager.register(YouTube(filereader.content))
+    component_manager.register(EquationEnvironment(filereader.content))
+    component_manager.register(Code(filereader.content))
+
+    # preprocess markdown file with customised converters
+    # they will already convert markdown to html
+    preprocessor = Preprocessor(filereader.content, component_manager.components)
+    preprocessor.preprocess_content()
+        
+    # convert remaining (standard) markdown to html
+    converter = Mark2HTML(preprocessor.processed_content)
+    html_content = converter.convert()    
+
+    # write output file
+    filereader.output(html_content)
+
+
+if __name__ == '__main__':
+    main()
