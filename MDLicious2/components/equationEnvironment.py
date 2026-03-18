079062f85e7472e06a6c8d78c47e09e80d89562a
tomrobin-teschner <tom@cfd.university>
2026-03-18 19:26:22 +0000

    added an equation component

---
+++ MDLicious2/components/equationEnvironment.py
@@ -0,0 +1,20 @@
+from MDLicious2.components.base import Component
+from os.path import join
+
+class EquationEnvironment(Component):
+    def __init__(self, content):
+        super().__init__(content)
+
+    def match(self, index):
+        line = self.content[index]
+        return (line.find('$$') != -1) and (len(line.strip()) == 2)
+
+    def convert(self, index):
+        start, end = self._find_start_end_based_on_pattern(index, '$$')
+
+        equation = ''
+        for i in range(start, end):
+            equation += '\n' + self.content[i]
+        equation += '\n'
+        
+        return f'<div class="wp-block-katex-display-block katex-eq" data-katex-display="true"><pre>{equation}</pre></div>'
