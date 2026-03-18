406a41e000fbad0ccb17e9670ff1f4c330eb8676
tomrobin-teschner <tomrobin.teschner@yahoo.de>
2026-03-18 19:29:40 +0000

    supplied __ini__ file to make it a package

---
+++ MDLicious2/__init__.py
@@ -0,0 +1,11 @@
+from MDLicious2.processor.cla import CommandLineArguments
+from MDLicious2.processor.fileProcessor import FileProcessor
+from MDLicious2.convertor.mark2html import Mark2HTML
+from MDLicious2.convertor.preprocessor import Preprocessor
+
+
+# components
+from MDLicious2.components.componentManager import ComponentManager
+from MDLicious2.components.youtube import YouTube
+from MDLicious2.components.equationEnvironment import EquationEnvironment 
+from MDLicious2.components.code import Code 
