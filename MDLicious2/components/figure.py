7c873e8d487234820241a6abc099e39488a3146e
tomrobin-teschner <tomrobin.teschner@yahoo.de>
2026-03-22 16:45:56 +0000

    made extracting captions its own class

--- MDLicious2/components/figure.py
+++ MDLicious2/components/figure.py
@@ -1,4 +1,5 @@
 from MDLicious2.components.base import Component
+from MDLicious2.components.captionExtractor import CaptionExtractor
 
 class Figure(Component):
     def __init__(self, content):
@@ -28,27 +29,11 @@ class Figure(Component):
         alt_text = next_line.split('![')[1].split('](')[0]
         image_source = next_line.split('](')[1].split(')')[0]
 
-        # check if captions need to be added
-        has_caption = line.find('caption:') != -1
-        if has_caption:
-            caption = line.split('caption: "')[1].split('"')[0]
+        caption = CaptionExtractor()
 
-            # check if caption contains a link
-            caption_has_link = True
-            while caption_has_link:
-                caption_has_link = (caption.find('[') != -1) and (caption.find('](') != -1)
-                if caption_has_link:
-                    link_text = caption.split('[')[1].split('](')[0]
-                    link_source = caption.split('](')[1].split(')')[0]
-                    html_link = f'<a href="{link_source}" target="_blank" rel="noopener noreferrer">{link_text}</a>'
-                    caption = caption.replace(f'[{link_text}]({link_source})', html_link)
-                else:
-                    break
-
-        figure_html = '<figure class="wp-block-image aligncenter size-large is-resized">'
+        figure_html = '<figure class="wp-block-image aligncenter size-large is-resized">\n'
         figure_html += f'<img src="{image_source}" alt="{alt_text}" class="wp-image-5550" style="width:{width}px"/>'
-        if has_caption:
-            figure_html += f'<figcaption class="wp-element-caption">{caption}</figcaption>'
+        figure_html += caption.extract(line)
         figure_html += '</figure>'
 
         return figure_html
